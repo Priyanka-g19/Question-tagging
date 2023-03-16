c679a8c6b88e8f97db447f19b6dbf4abca2885d0
Priyamka-g19 <priyankagupta8880@gmail.com>
2023-03-17 01:51:26 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,79 @@
+import openai,os,json,yaml,sys
+from flask import Flask,request,jsonify,render_template
+from ratelimit import limits,sleep_and_retry
+#from logger import logging
+#from exception import customException
+
+
+app=Flask(__name__)
+global config
+with open("app.yaml","r") as f:
+    config= yaml.safe_load(f)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+# Define rate limiting parameters
+RATE_LIMIT = config["RATE_LIMIT"]  # maximum number of requests per minute
+RATE_PERIOD = config["RATE_PERIOD"]  # time period in seconds for rate limit
+
+# Define rate limiter decorator
+@sleep_and_retry
+@limits(calls=RATE_LIMIT, period=RATE_PERIOD)
+@app.route("/chatgpt",methods=["GET","POST"])
+def chat_gpt():
+    #logging.info("Entered the chatgpt function")
+    if request.method == "GET" or request.method == "POST":
+        try:
+            prompt='Identify the subject, topic,subtopic,  difficulty level,blooms taxonomy level for the question only. Q. '
+            #logging.info("Taking the prompt as Question from the user")
+            prompt += request.args.get("prompt")
+            print(prompt)
+            api_key = os.environ.get("API_KEY")
+
+            openai.api_key=api_key
+            #logging.info("Running the chatgpt api")
+
+            response= openai.ChatCompletion.create(
+                 model="gpt-3.5-turbo",
+                 messages=[{"role": "user", "content": prompt}],
+                 n=1,
+                 stop=None,
+                 temperature = 0.7,
+                 presence_penalty=0
+            )
+            #logging.info("storing the output from chat gpt")
+            text = response['choices'][0]['message']['content'].strip()
+
+            # return jsonify(
+            #     OUTPUT = prepare_json(text)
+            # )
+            #
+            #logging.info("Returning the output from chatpt api as json in html")
+            return render_template("index.html",output=prepare_json(text))
+        except Exception as e:
+            return jsonify(
+                ERROR = f"{e}"
+            )
+        
+
+def  prepare_json(output):
+    #logging.info("Entered the method to convert chatgpt text response to json response")
+    #logging.info("Converting chatgpt response to list")
+    lst=output.split('\n')
+    #logging.info("Creating lists within the list to separate group subject,topic,subtopic,blooms taxonomy anf difficulty level separately")
+    new_lst=[lst[i].split(':') for i in range(0,len(lst))]
+    out = {}
+    #logging.info("Converting nested list into dictionary")
+    for val in new_lst:
+        if len(val) > 0:
+            out[val[0]] = val[1].split(",")
+    #logging.info("Converting dictionary to json")
+    return json.dumps(out)
+
+
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0",port=5000,debug=True)
